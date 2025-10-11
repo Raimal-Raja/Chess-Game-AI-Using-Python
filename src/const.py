0a084a030ef3233b4b4029b49d9ffee848173586
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-10-11 21:31:32 +0500

    updated

---
+++ src/const.py
@@ -0,0 +1,9 @@
+# Screen Dimensions
+WIDTH = 800
+HEIGHT = 800
+
+#Board Dimension
+
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH//HEIGHT
---
+++ src/sqaure.py
@@ -0,0 +1,5 @@
+class Sqaure:
+    def __init__(self, row, col, piecs=None):
+        self.row = row
+        self.col = col
+        self.piece = piece
