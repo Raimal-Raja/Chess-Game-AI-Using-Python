dc91dcbc4d7a12c3088758b4388e39b872d405b7
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-10-15 03:52:22 +0500

    updated

--- src/sound.py
+++ src/sound.py
@@ -1,10 +1,19 @@
 import pygame
+import sys
+import os
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 class Sound:
 
     def __init__(self, path):
-        self.path = path
-        self.sound = pygame.mixer.Sound(path)
+        self.path = resource_path(path)
+        self.sound = pygame.mixer.Sound(self.path)
 
     def play(self):
         pygame.mixer.Sound.play(self.sound)
